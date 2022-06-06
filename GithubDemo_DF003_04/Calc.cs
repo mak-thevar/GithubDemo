bf21ac43f36eb29020e789bd184c730892a0f7bb
Mak Thevar <mak.thevar11@gmail.com>
2022-06-06 16:18:54 +0530

    Inital commit

---
+++ GithubDemo_DF003_04/Calc.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GithubDemo_DF003_04
+{
+
+    public interface ICalc
+    {
+        int Add(int x, int y);
+        int Multiply(int x, int y);
+        int Divide(int x, int y);
+        int Subtract(int x, int y);
+    }
+
+    public class Calc : ICalc
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int Divide(int x, int y)
+        {
+            return x / y;
+        }
+
+        public int Multiply(int x, int y)
+        {
+            return (x * y);
+        }
+
+        public int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+    }
+}
---
+++ GithubDemo_DF003_04/Program.cs
@@ -0,0 +1,30 @@
+﻿using GithubDemo_DF003_04;
+using System.Reflection;
+
+var methods = typeof(ICalc).GetMethods();
+
+Console.WriteLine("Choose the options from below :");
+foreach (var item in methods)
+{
+    Console.WriteLine(item.Name);
+}
+
+Console.WriteLine("----------------------------------------------------\n");
+
+var theOption = Console.ReadLine();
+
+Console.WriteLine("Enter the value for x and y as comma sperated eg. 20,30");
+var theValues = Console.ReadLine();
+var strx = theValues.Split(',')[0];
+var stry = theValues.Split(',')[1];
+var x = Convert.ToInt32(strx);
+var y = Convert.ToInt32(stry);
+var calc = new Calc();
+
+var methodInfo = methods.Where(m => m.Name.Normalize() == theOption.Normalize()).FirstOrDefault();
+if (methodInfo is not null)
+{
+    var result = methodInfo.Invoke(calc, new object[] { x, y });
+    Console.WriteLine("The result is {0}",result);
+}
+Console.ReadLine();
